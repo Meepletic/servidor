88d64a1bd3697956d68531208a023272dcf0afe7
Galán <antoniogalanG15@gmail.com>
2022-05-21 10:55:04 +0200

    Proyecto Spring Boot vacío añadido

---
+++ src/main/java/com/meepletic/servidor/Servidor6Application.java
@@ -0,0 +1,13 @@
+package com.meepletic.servidor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Servidor6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Servidor6Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/meepletic/servidor/Servidor6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meepletic.servidor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Servidor6ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
