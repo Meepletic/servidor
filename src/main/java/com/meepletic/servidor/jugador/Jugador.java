78a1461a861ecabd730222b0061f7d9e8ef96b83
Galán <antoniogalanG15@gmail.com>
2022-05-21 11:05:56 +0200

    Modelo del Jugador creado

---
+++ src/main/java/com/meepletic/servidor/jugador/Jugador.java
@@ -0,0 +1,60 @@
+package com.meepletic.servidor.jugador;
+
+public class Jugador {
+
+    private long id;
+    private String nombre;
+    private String usuario;
+
+
+    // Constructores
+
+    public Jugador() {
+
+    }
+
+    public Jugador(long id, String nombre, String usuario) {
+        super();
+        this.id = id;
+        this.nombre = nombre;
+        this.usuario = usuario;
+    }
+
+
+    // Getters
+
+    public long getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+
+    // Setters
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+
+    // Métodos
+
+    @Override
+    public String toString() {
+        return "Jugador #" + id + ": " + nombre + " (" + usuario + ")";
+    }
+}
